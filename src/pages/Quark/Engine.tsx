83e6edf2288038d29cefa8d8bda746b842f8a999
tangtanglove <dai_hang_love@126.com>
2020-04-17 18:48:06 +0800

    提交代码

--- src/pages/Quark/Engine.tsx
+++ src/pages/Quark/Engine.tsx
@@ -3,6 +3,7 @@ import { connect } from 'dva';
 import FormPage from '@/components/QuarkUI/FormPage';
 import TablePage from '@/components/QuarkUI/TablePage';
 import ShowPage from '@/components/QuarkUI/ShowPage';
+import DashboardPage from '@/components/QuarkUI/DashboardPage';
 import { stringify } from 'qs';
 import zhCN from 'antd/es/locale/zh_CN';
 
@@ -31,12 +32,14 @@ class Engine extends PureComponent<any> {
               {!!engine.component && engine.component =='form' ? <FormPage api={engine.api} type={'page'} search={engine.search} /> : null}
               {!!engine.component && engine.component =='table' ? <TablePage api={engine.api} search={engine.search} /> : null}
               {!!engine.component && engine.component =='show' ? <ShowPage api={engine.api} search={engine.search} /> : null}
+              {!!engine.component && engine.component =='dashboard' ? <DashboardPage api={engine.api} search={engine.search} /> : null}
             </div>
           : 
             <div>
               {!!this.state.component && this.state.component =='form' ? <FormPage api={this.state.api} type={'page'} search={this.state.search} /> : null}
               {!!this.state.component && this.state.component =='table' ? <TablePage api={this.state.api} search={this.state.search} /> : null}
               {!!this.state.component && this.state.component =='show' ? <ShowPage api={this.state.api} search={this.state.search} /> : null}
+              {!!this.state.component && this.state.component =='dashboard' ? <DashboardPage api={this.state.api} search={this.state.search} /> : null}
             </div>
           }
         </div>
