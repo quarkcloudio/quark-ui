df02d98f1c95be13a7e87787a2d28a876a6cf019
tangtanglove <dai_hang_love@126.com>
2020-10-04 13:50:19 +0800

    提交代码

--- src/pages/Quark/components/Form.tsx
+++ src/pages/Quark/components/Form.tsx
@@ -13,54 +13,48 @@ export interface Table {
 }
 
 const Form: React.FC<Table> = (props) => {
-  const actionRef = useRef<any>(undefined);
+
+  // 解析表单item
+  const formItemRender = (items:any) => {
+    const formItemComponent = (
+      items.map((item:any,key:any) => {
+        let component:any = null;
+        switch (item.component) {
+          case 'text':
+            component = 
+            <ProFormText
+              key={item.key}
+              name={item.name}
+              label={item.label}
+              tooltip={item.tooltip}
+              placeholder={item.placeholder}
+            />;
+            break;
+
+          default:
+            break;
+        }
+        return component;
+      })
+    )
+    return formItemComponent;
+  }
 
   return (
     <ProForm
-      onFinish={async (values) => {
-
-      }}
+      style={props.form.style}
+      colon={props.form.colon}
+      initialValues={props.form.initialValues}
+      labelAlign={props.form.labelAlign}
+      name={props.form.name}
+      preserve={props.form.preserve}
+      requiredMark={props.form.requiredMark}
+      scrollToFirstError={props.form.scrollToFirstError}
+      size={props.form.size}
+      dateFormatter={props.form.dateFormatter}
+      layout={props.form.layout}
     >
-      <ProForm.Group>
-        <ProFormText
-          name="name"
-          label="签约客户名称"
-          tooltip="最长为 24 位"
-          placeholder="请输入名称"
-        />
-        <ProFormText name="company" label="我方公司名称" placeholder="请输入名称" />
-      </ProForm.Group>
-      <ProForm.Group>
-        <ProFormText name="contract" label="合同名称" placeholder="请输入名称" />
-        <ProFormDateRangePicker name="contractTime" label="合同生效时间" />
-      </ProForm.Group>
-      <ProForm.Group>
-        <ProFormSelect
-          options={[
-            {
-              value: 'chapter',
-              label: '盖章后生效',
-            },
-          ]}
-          width="xs"
-          name="useMode"
-          label="合同约定生效方式"
-        />
-        <ProFormSelect
-          width="xs"
-          options={[
-            {
-              value: 'time',
-              label: '履行完终止',
-            },
-          ]}
-          name="unusedMode"
-          label="合同约定失效效方式"
-        />
-      </ProForm.Group>
-      <ProFormText width="s" name="id" label="主合同编号" />
-      <ProFormText name="project" disabled label="项目名称" initialValue="xxxx项目" />
-      <ProFormText width="xs" name="mangerName" disabled label="商务经理" initialValue="启途" />
+      {formItemRender(props.form.items)}
     </ProForm>
   );
 }
