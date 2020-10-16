390c4881a1121f23c35a012c738577f5e30043ad
tangtanglove <l>
2020-10-16 18:50:13 +0800

    提交代码

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -32,22 +32,7 @@ const GlobalHeaderRight: React.FC<{}> = () => {
       <HeaderSearch
         className={`${styles.action} ${styles.search}`}
         placeholder="站内搜索"
-        defaultValue="umi ui"
-        options={[
-          { label: <a href="https://umijs.org/zh/guide/umi-ui.html">umi ui</a>, value: 'umi ui' },
-          {
-            label: <a href="next.ant.design">Ant Design</a>,
-            value: 'Ant Design',
-          },
-          {
-            label: <a href="https://protable.ant.design/">Pro Table</a>,
-            value: 'Pro Table',
-          },
-          {
-            label: <a href="https://prolayout.ant.design/">Pro Layout</a>,
-            value: 'Pro Layout',
-          },
-        ]}
+        options={[]}
         // onSearch={value => {
         //   //console.log('input', value);
         // }}
@@ -56,7 +41,7 @@ const GlobalHeaderRight: React.FC<{}> = () => {
         <span
           className={styles.action}
           onClick={() => {
-            window.location.href = 'https://pro.ant.design/docs/getting-started';
+            window.location.href = 'http://www.quarkcms.com/';
           }}
         >
           <QuestionCircleOutlined />
