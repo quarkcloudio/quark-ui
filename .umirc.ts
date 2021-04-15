490c96d63c9a797692a2ea3a72795863825eb209
quarkcms <60868863+quarkcms@users.noreply.github.com>
2021-04-15 17:18:51 +0800

    提交代码

--- .umirc.ts
+++ .umirc.ts
@@ -4,9 +4,18 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  proxy: {
+    '/api/': {
+      target: 'http://quark-admin.test',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '/api' },
+    },
+  },
+  publicPath: './',
+  history: { type: 'hash' },
   routes: [
-    { path: '/', component: '@/pages/index' },
-    { path: '/index', component: '@/pages/index' },
+    { path: '/', component: '@/pages/Index/index' },
+    { path: '/index', component: '@/pages/Index/index' },
   ],
   fastRefresh: {},
 });
