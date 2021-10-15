7fd04dfc42ccd4a4aa3eef853ccd92a47eb26a67
tangtanglove <dai_hang_love@126.com>
2021-10-15 13:08:08 +0800

    wip

--- src/components/Form/Cascader.tsx
+++ src/components/Form/Cascader.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Cascader as AntCascader } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Cascader as AntCascader, Spin } from 'antd';
 import { get } from '@/services/action';
 
 export interface Search {
@@ -15,9 +15,48 @@ export interface Search {
 
 const Cascader: React.FC<Search> = ({ api=null, size=undefined, value=null, placeholder=null, style=[], options=[],allowClear=false, onChange }) => {
   const [selectOptions, setSelectOptions] = useState(options);
-  //hack
+  const [spinning, setSpinning] = useState(true);
   const [random, setRandom] = useState(0);
   
+  useEffect(() => {
+    initOptions();
+  }, []);
+
+  const initOptions = async () => {
+    if(api) {
+      const getOptions = await loadOptions();
+      setSelectOptions(getOptions);
+
+      setTimeout(async () => {
+        setSpinning(false);
+      }, 3000);
+    }
+  };
+
+  const loadOptions = async (level:any = 0) => {
+
+    const result = await get({
+      actionUrl: api,
+      search: level===0 ? 0 : value[level-1],
+      level: level,
+    });
+
+    if(value) {
+
+      if(level < value.length) {
+        result?.data?.map(async (item:any) => {
+          if(item.value === value[level]) {
+            return item['children'] = await loadOptions(level+1);
+          }
+        })
+      }
+
+      return result['data'];
+    } else {
+      return result['data'];
+    }
+  };
+
   const triggerChange = (changedValue:any) => {
     if (onChange) {
       onChange(changedValue);
@@ -50,17 +89,19 @@ const Cascader: React.FC<Search> = ({ api=null, size=undefined, value=null, plac
 
   if(api) {
     return (
-      <AntCascader
-        size={size}
-        loadData={loadData}
-        placeholder={placeholder}
-        style={style}
-        allowClear={allowClear}
-        onChange={onSelectChange}
-        value={value}
-        options={getSelectOptions}
-        changeOnSelect
-      />
+      <Spin spinning={spinning} size="small">
+        <AntCascader
+          size={size}
+          loadData={loadData}
+          placeholder={placeholder}
+          style={style}
+          allowClear={allowClear}
+          onChange={onSelectChange}
+          value={value}
+          options={getSelectOptions}
+          changeOnSelect
+        />
+      </Spin>
     );
   } else {
     return (
