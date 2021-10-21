32b0e35442acb631d25a171a57db05fd42c7bd4f
tangtanglove <dai_hang_love@126.com>
2021-10-21 17:19:27 +0800

    wip

--- src/components/Display/Descriptions.tsx
+++ src/components/Display/Descriptions.tsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import ProDescriptions from '@ant-design/pro-descriptions';
 import Render from '@/components/Render';
+import Action from '@/components/Action/Action';
+import {
+  Space,
+  Divider
+} from 'antd';
 
-const Descriptions: React.FC<any> = (props:any) => {
-  const componentRender = (items:any) => {
-    return items.map((item:any) => {
-        return <ProDescriptions.Item key={item.key} label={item.label}>
-                <Render body={{...item, body:item.value}} data={props.data}/>
-              </ProDescriptions.Item>;
-      })
-  }
+const Descriptions: React.FC<any> = (props: any) => {
+  const componentRender = (items: any) => {
+    return items?.map((item: any) => {
+      return (
+        <ProDescriptions.Item key={item.key} label={item.label}>
+          <Render body={{ ...item, body: item.value }} data={props.data} />
+        </ProDescriptions.Item>
+      );
+    });
+  };
 
   return (
-    <ProDescriptions {...props}>
-      {componentRender(props.items)}
-    </ProDescriptions>
+    <span>
+      <ProDescriptions {...props}>{componentRender(props.items)}</ProDescriptions>
+      {
+        props?.actions?
+          <div>
+            <Divider style={{marginTop:0}}/>
+            <div style={{marginBottom:24,textAlign:'center'}}>
+              <Space>
+                {props?.actions?.map((action:any) => {
+                  return <Action {...action} data={{...props.data, ...props.dataSource}} callback={props.callback}/>
+                })}
+              </Space>
+            </div>
+          </div>
+        : null
+      }
+    </span>
   );
-}
+};
 
 export default Descriptions;
