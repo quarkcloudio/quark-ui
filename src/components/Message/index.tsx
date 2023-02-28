f4824a9e026d96e803b863eb7423549587f67539
tangtanglove <dai_hang_love@126.com>
2023-02-28 13:57:52 +0800

    fix: 新增Message组件

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,33 @@
+import { message } from 'antd';
+import { history } from '@umijs/max';
+
+const Message: any = (props:any, successCallback:any = null) => {
+  const [messageApi] = message.useMessage();
+  if (props.component !== 'message') {
+    return false
+  }
+
+  if (props.content) {
+    messageApi.open({
+      type: props.type,
+      content: props.content,
+    });
+  }
+
+  if (props.url) {
+    history.push(props.url);
+  }
+
+  if (props.callback) {
+    props.callback();
+  }
+
+  // 成功后回调
+  if (props.type == "success") {
+    if (successCallback) {
+      successCallback()
+    }
+  }
+};
+
+export default Message;
