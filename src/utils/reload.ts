2b820bb3b107ad9a53f6d80fcced1085bafd91df
tangtanglove <dai_hang_love@126.com>
2024-12-05 08:31:05 +0800

    chore: wip

--- src/utils/reload.ts
+++ src/utils/reload.ts
@@ -1,13 +1,19 @@
 import { history } from '@umijs/max';
 
 const useReload = () => {
-  const { search }: any = history.location;
+  let { search }: any = history.location;
   const timestamp = new Date().getTime().toString();
+
+  if (search.indexOf('timestamp') > -1) {
+    search = search.replace(/timestamp=(\d*)/, `timestamp=${timestamp}`);
+  } else {
+    search += '&timestamp=' + timestamp;
+  }
 
   // @ts-ignore
   history.push({
     pathname: history.location.pathname,
-    search: search + '&timestamp=' + timestamp,
+    search: search,
   });
 };
 
