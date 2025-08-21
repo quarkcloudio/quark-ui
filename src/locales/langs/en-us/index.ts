c2578d67fd0e52e92f3dbd8efcc39238c9e0750e
tangtanglove <dai_hang_love@126.com>
2025-08-21 17:34:40 +0800

    chore: 更正语言包

--- src/locales/langs/en-us/index.ts
+++ src/locales/langs/en-us/index.ts
@@ -36,7 +36,7 @@ const local: App.I18n.Schema['translation'] = {
   system: {
     errorReason: 'Cause Error',
     reload: 'Reload Page',
-    title: 'SoybeanAdmin',
+    title: 'QuarkUIAdmin',
     updateCancel: 'Later',
     updateConfirm: 'Refresh immediately',
     updateContent: 'A new version of the system has been detected. Do you want to refresh the page immediately?',
--- src/locales/langs/zh-cn/index.ts
+++ src/locales/langs/zh-cn/index.ts
@@ -42,7 +42,7 @@ const local: App.I18n.Schema['translation'] = {
   system: {
     errorReason: '错误原因',
     reload: '重新渲染页面',
-    title: 'Soybean 管理系统',
+    title: 'QuarkUI 管理系统',
     updateCancel: '稍后再说',
     updateConfirm: '立即刷新',
     updateContent: '检测到系统有新版本发布，是否立即刷新页面？',
