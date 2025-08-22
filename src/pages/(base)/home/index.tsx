6d0a079b1b684d1759f8999258fe94c0810a3216
tangtanglove <dai_hang_love@126.com>
2025-08-22 17:26:50 +0800

    chore: wip

--- src/pages/(base)/home/index.tsx
+++ src/pages/(base)/home/index.tsx
@@ -1,9 +1,6 @@
 import CardData from './modules/CardData';
-import CreativityBanner from './modules/CreativityBanner';
-import HeaderBanner from './modules/HeaderBanner';
 import LineChart from './modules/LineChart';
 import PieChart from './modules/PieChart';
-import ProjectNews from './modules/ProjectNews';
 
 const Home = () => {
   return (
@@ -12,8 +9,6 @@ const Home = () => {
       direction="vertical"
       size={[16, 16]}
     >
-      <HeaderBanner />
-
       <CardData />
 
       <ARow gutter={[16, 16]}>
@@ -28,20 +23,6 @@ const Home = () => {
           span={24}
         >
           <PieChart />
-        </ACol>
-      </ARow>
-      <ARow gutter={[16, 16]}>
-        <ACol
-          lg={14}
-          span={24}
-        >
-          <ProjectNews />
-        </ACol>
-        <ACol
-          lg={10}
-          span={24}
-        >
-          <CreativityBanner />
         </ACol>
       </ARow>
     </ASpace>
