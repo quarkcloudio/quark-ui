a2d9ee20375ce132843accef515ec2f47f775c26
tangtanglove <dai_hang_love@126.com>
2025-08-26 19:04:29 +0800

    chore: 统一登录样式

--- src/pages/(blank)/login/layout.tsx
+++ src/pages/(blank)/login/layout.tsx
@@ -36,7 +36,8 @@ const LoginLayout = () => {
       <WaveBg themeColor={bgThemeColor} />
 
       <ACard
-        className="relative z-4 w-auto rd-12px"
+        className="relative z-4 w-auto rd-8px"
+        styles={{ body: { flex: 1, overflow: 'hidden', padding: undefined } }}
         variant="borderless"
       >
         <div className="w-400px lt-sm:w-300px">
