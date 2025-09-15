4ab40a261d6c3509f4c91470401b54d6ef8c630e
tangtanglove <dai_hang_love@126.com>
2025-09-15 17:55:34 +0800

    chore: wip

--- src/components/pro-table/ProTable.tsx
+++ src/components/pro-table/ProTable.tsx
@@ -1,5 +1,75 @@
-const ProTable = () => {
-  return <div>ProTable</div>;
+import type { TableProps } from 'antd';
+
+interface ProTableProps {
+  api?: string;
+}
+
+interface DataType {
+  address: string;
+  age: number;
+  key: string;
+  name: string;
+}
+
+const ProTable = (props: ProTableProps) => {
+  console.log(props);
+
+  const columns: TableProps<DataType>['columns'] = [
+    {
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a>{text}</a>,
+      title: 'Name'
+    },
+    {
+      dataIndex: 'age',
+      key: 'age',
+      title: 'Age'
+    },
+    {
+      dataIndex: 'address',
+      key: 'address',
+      title: 'Address'
+    },
+    {
+      key: 'action',
+      render: (_, record) => (
+        <ASpace size="middle">
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </ASpace>
+      ),
+      title: 'Action'
+    }
+  ];
+
+  const data: DataType[] = [
+    {
+      address: 'New York No. 1 Lake Park',
+      age: 32,
+      key: '1',
+      name: 'John Brown'
+    },
+    {
+      address: 'London No. 1 Lake Park',
+      age: 42,
+      key: '2',
+      name: 'Jim Green'
+    },
+    {
+      address: 'Sydney No. 1 Lake Park',
+      age: 32,
+      key: '3',
+      name: 'Joe Black'
+    }
+  ];
+
+  return (
+    <ATable<DataType>
+      columns={columns}
+      dataSource={data}
+    />
+  );
 };
 
 export default ProTable;
