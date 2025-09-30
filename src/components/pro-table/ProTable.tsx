98fdaf8b4a0630fb11b0c3193e293c90e28e8e39
tangtanglove <dai_hang_love@126.com>
2025-09-30 18:32:34 +0800

    chore: 完善Table组件

--- src/components/pro-table/ProTable.tsx
+++ src/components/pro-table/ProTable.tsx
@@ -191,7 +191,6 @@ const ProTable = (props: ProTableProps) => {
             />
             <ProTableHeaderOperation
               columns={columnChecks}
-              loading={loading}
               refresh={() => onRequest()}
               setColumnChecks={setColumnChecks}
             />
