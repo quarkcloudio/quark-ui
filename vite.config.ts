c877af83316ff9fed70ea3d8412b7f4e4c0691b3
tangtanglove <dai_hang_love@126.com>
2025-11-01 16:29:17 +0800

    chore: 优化代码

--- vite.config.ts
+++ vite.config.ts
@@ -85,7 +85,7 @@ export default defineConfig(configEnv => {
     },
     plugins: setupVitePlugins(viteEnv, buildTime),
     preview: {
-      port: 8000
+      port: 8174
     },
     resolve: {
       alias: {
@@ -96,7 +96,7 @@ export default defineConfig(configEnv => {
     server: {
       host: '0.0.0.0',
       open: true,
-      port: 8000,
+      port: 8174,
       proxy: createViteProxy(viteEnv, enableProxy),
       warmup: {
         clientFiles: ['./index.html', './src/{pages,components}/*']
